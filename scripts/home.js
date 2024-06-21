3a8164f2ad347695e2bbc1f447e84f71c5f13ec6
kellenstrydom <131270586+kellenstrydom@users.noreply.github.com>
2024-06-21 17:11:07 +0200

    cookies start

--- scripts/home.js
+++ scripts/home.js
@@ -1,27 +1,56 @@
-fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
-.then((r) => r.json())
-.then((fact) => {
-    console.log(fact)
-    //randomFact(fact)
-})
+// if (typeof(Storage) !== "undefined") 
+// {
+//   if (localStorage.homedisplay) {
+//     if (homedisplay == 'A')
+//     {
+//       DisplayAdvice();
+//     }
+//     else{
+//       DisplayFact();
+//     }
+//   } 
+//   else {
+//     localStorage.clickcount = 1;
+//   }
+//   document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+// } 
+// else {
+//   document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+// }
 
 
-.catch((error) => console.log("The error:",error));
-fetch('https://api.adviceslip.com/advice')
+
+function DisplayFact(){
+  fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+  .then((r) => r.json())
+  .then((fact) => {
+      console.log(fact)
+      DisplayText(fact.text)
+  })
+  .catch((error) => console.log("The error:",error));
+}
+
+function DisplayAdvice(){
+  fetch('https://api.adviceslip.com/advice')
   .then(response => response.json())
   .then(data => {
     const advice = data.slip.advice;
     console.log(`Advice: ${advice}`);
-    document.querySelector("div").innerText = advice;
+    //document.querySelector("div").innerText = advice;
+    DisplayText(advice);
   });
-
-
-const randomFact = (fact) => {
-    const div = document.querySelector("div");
-    const a = document.createElement("a");
-    a.innerText = fact.text;
-    a.setAttribute("href", fact.source_url);
-    a.setAttribute("target", "_blank");
-    div.appendChild(a);
 }
+
+
+
+function DisplayText(text){
+  const div = document.querySelector("div");
+  const textbox = document.createElement("section");
+  textbox.className = "textbox"
+  textbox.innerText = text;
+  div.appendChild(textbox);
+}
+
+//DisplayAdvice();
+DisplayFact();
  
